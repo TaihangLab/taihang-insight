50968c331df6d022b372f55de9d3b6f48386abfb
bailingnan <lingnanbai@gmail.com>
2026-05-27 18:32:39 +0800

    refactor:刪除无用文件

--- test_axios.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios');
-axios.defaults.baseURL = 'http://192.168.1.1:8000/api/v1/wvp';
-
-axios.get('/api/v1/wvp/api/push/start').catch(e => console.log(e.config.url, e.request?._currentUrl));
-axios.get('api/v1/wvp/api/push/start').catch(e => console.log(e.config.url, e.request?._currentUrl));
-axios.get('/api/v1/region/list').catch(e => console.log(e.config.url, e.request?._currentUrl));
