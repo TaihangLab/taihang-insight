bd15a1504946088a82f6b282f5b06c4c35d60c3c
liyizheng <2873492161@qq.com>
2026-07-31 15:39:11 +0800

    feat:修改接口参数问题

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ const path = require('path')
 // 后端API基础地址配置
 // const API_BASE_URL = 'http://127.0.0.1:8000';//本地测试地址
 // const API_BASE_URL = 'https://172.16.201.80/prod-api/smart-engine';//测试环境综治平台地址
-const API_BASE_URL = 'http://172.17.44.123:81/prod-api/smart-engine' ; //生产环境综治平台地址
+const API_BASE_URL = 'http://172.17.44.123:81/prod-api/smart-engine'; //生产环境综治平台地址
 
 module.exports = {
   // 导出API配置供前端使用
