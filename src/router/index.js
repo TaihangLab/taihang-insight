de3f5038aaa0a914ac92b2c06da97fd2a8236d1e
bailingnan <lingnanbai@gmail.com>
2026-08-28 16:09:10 +0800

    bugFix:redirect到智算中心页面

--- src/router/index.js
+++ src/router/index.js
@@ -57,7 +57,7 @@ export default new VueRouter({
       path: '/',
       name: 'home',
       component: Layout,
-      redirect: '/visualCenter',
+      redirect: '/algorithmInference',
       children: [
 
 
